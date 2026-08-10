45beba454083933320cc5cae60fba16384169782
seathrun-code <seathrun@gmail.com>
2026-08-10 21:02:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -117,6 +117,7 @@ st.write(f"Ag taispeáint {len(filtered_df)} taifead")
 event = st.dataframe(
     filtered_df.style.hide(axis="index"), 
     use_container_width=True,
+    hide_index=True,
     on_select="rerun",
     selection_mode="single-row"
 )
